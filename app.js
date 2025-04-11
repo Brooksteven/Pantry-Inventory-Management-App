0185b075d8edbe55a4b514cad35bf9d53f5a11c4
Brooklynn Stevenson <brooksteven580@gmail.com>
2025-04-11 14:47:57 -0400

    added custom middleware to app.js and initilized it as well as added middleware files logging and requestTime to middleware folder to keep code organized and clean

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const loggingMiddleware = require('./middleware/loggingMiddleware');
+const requestTimeMiddleware = require('./middleware/requestTimeMiddleware');
 const port = 3000
 
 //Create and render at least one view using a view template and template engine. Third-party Engine. (8%)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
+
+
+//Create and use at least two pieces of custom middleware (5%)
+app.use(loggingMiddleware);
+app.use(requestTimeMiddleware);
+app.use('/users', userRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
