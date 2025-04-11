a65a71346d041a86ecb8aab7ec4129ebe476aea9
Brooklynn Stevenson <brooksteven580@gmail.com>
2025-04-11 14:24:39 -0400

    Created and rendered view (index.ejs) using a view template and template engine (ejs).

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const path = require('path');
 const app = express();
 const port = 3000
 
-
+//Create and render at least one view using a view template and template engine. Third-party Engine. (8%)
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
