6d32aab4b1d4c34732d45982170e7952d0a0addf
Brooklynn Stevenson <brooksteven580@gmail.com>
2025-04-11 15:09:14 -0400

    added data categories and routes and then added the routes/middleware through app.use and then listed the imports for all routes and middleware for data

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const loggingMiddleware = require('./middleware/loggingMiddleware');
 const requestTimeMiddleware = require('./middleware/requestTimeMiddleware');
 const port = 3000
 
+//Import Routes
+const itemRoutes = require('./routes/items');
+const userRoutes = require('./routes/users')
+
 //Create and render at least one view using a view template and template engine. Third-party Engine. (8%)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -14,6 +18,10 @@ app.set('views', path.join(__dirname, 'views'));
 //Create and use at least two pieces of custom middleware (5%)
 app.use(loggingMiddleware);
 app.use(requestTimeMiddleware);
+
+
+//Routes
+app.use('/items', itemRoutes);
 app.use('/users', userRoutes);
 
 app.listen(PORT, () => {
